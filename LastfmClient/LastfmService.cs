75d5243245021ee104be56e9ed7ba895f75109a2
Anthony Sciamanna <asciamanna@gmail.com>
2014-08-17 07:17:03 -0400

    getting rid of unused dependencies from service class.

--- LastfmClient/LastfmService.cs
+++ LastfmClient/LastfmService.cs
@@ -14,24 +14,18 @@ namespace LastfmClient {
   }
 
   public class LastfmService : ILastfmService {
-    readonly IRestClient restClient;
-    readonly ILastfmResponseParser parser;
-    readonly IPageCalculator pageCalculator;
     readonly ILastfmPageScraper pageScraper;
     readonly IRepositoryFactory repositoryFactory;
     readonly string apiKey;
     const string lastfmUserPageUrl = @"http://www.last.fm/user/";
 
-    public LastfmService(string apiKey) : this(apiKey, new RestClient(), new LastfmResponseParser(), new PageCalculator(), new LastfmPageScraper(), new RepositoryFactory()) { }
+    public LastfmService(string apiKey) : this(apiKey, new LastfmPageScraper(), new RepositoryFactory()) { }
 
-    public LastfmService(string apiKey, IRestClient restClient, ILastfmResponseParser parser, IPageCalculator pageCalculator, ILastfmPageScraper pageScraper, IRepositoryFactory repositoryFactory) {
+    public LastfmService(string apiKey, ILastfmPageScraper pageScraper, IRepositoryFactory repositoryFactory) {
       if (string.IsNullOrEmpty(apiKey)) {
         throw new ArgumentException("An API key is required");
       }
       this.apiKey = apiKey;
-      this.restClient = restClient;
-      this.parser = parser;
-      this.pageCalculator = pageCalculator;
       this.pageScraper = pageScraper;
       this.repositoryFactory = repositoryFactory;
     }
@@ -60,10 +54,6 @@ namespace LastfmClient {
 
     public LastfmPlayingFrom FindCurrentlyPlayingFrom(string user) {
       return pageScraper.GetLastfmPlayingFromInfo(lastfmUserPageUrl + user);
-    }
-
-    string BuildUri(string baseUri, string user, int page) {
-      return string.Format(baseUri, apiKey, user, page);
     }
   }
 }
--- LastfmClientTests/LastfmServiceTest.cs
+++ LastfmClientTests/LastfmServiceTest.cs
@@ -12,28 +12,28 @@ namespace LastfmClientTests {
   public class LastfmServiceTest {
     [Test]
     public void Object_Constructed_Without_An_API_Key_Throws_Exception() {
-      var exception = Assert.Throws<ArgumentException>(() => new LastfmService(string.Empty, null, null, null, null, null));
+      var exception = Assert.Throws<ArgumentException>(() => new LastfmService(string.Empty, null, null));
       Assert.That(exception.Message, Is.EqualTo("An API key is required"));
     }
 
     [TestCase(-5)]
     [TestCase(0)]
     public void FindRecentTracks_Only_Takes_Positive_Values_For_Number_Of_Recent_Tracks(int numberOfTracks) {
-      var exception = Assert.Throws<ArgumentException>(() => new LastfmService("key", null, null, null, null, null).FindRecentTracks("me", numberOfTracks));
+      var exception = Assert.Throws<ArgumentException>(() => new LastfmService("key", null, null).FindRecentTracks("me", numberOfTracks));
       Assert.That(exception.Message, Is.EqualTo("numberOfTracks must be a positive integer."));
     }
 
     [TestCase(-11)]
     [TestCase(0)]
     public void FindTopArtists_Only_Takes_Positive_Values_For_Number_Of_Top_Artists(int numberOfArtists) {
-      var exception = Assert.Throws<ArgumentException>(() => new LastfmService("key", null, null, null, null, null).FindTopArtists("me", numberOfArtists));
+      var exception = Assert.Throws<ArgumentException>(() => new LastfmService("key", null, null).FindTopArtists("me", numberOfArtists));
       Assert.That(exception.Message, Is.EqualTo("numberOfArtists must be a positive integer."));
     }
 
     [Test]
     public void FindCurrentlyPlayingFrom_Delegates_To_PageScraper() {
       var pageScraper = MockRepository.GenerateStub<ILastfmPageScraper>();
-      var service = new LastfmService("key", null, null, null, pageScraper, null);
+      var service = new LastfmService("key", pageScraper, null);
       var user = "testingUser";
       var scraperResult = new LastfmPlayingFrom { MusicServiceName = "Spotify", MusicServiceUrl = @"http://www.spotify.com" };
 
