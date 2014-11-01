142539461f582c833754534bce16521184c53f40
Anthony Sciamanna <asciamanna@gmail.com>
2014-11-01 06:49:37 -0400

    adding custom lastfm exception to throw when lastfm returns an error

---
+++ LastfmClient/LastfmException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LastfmClient {
+  public class LastfmException : Exception {
+    public LastfmException() { }
+
+    public LastfmException(string message) : base(message) { }
+
+    public LastfmException(string message, Exception inner) : base(message, inner) { }
+  }
+}
