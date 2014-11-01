d8481b133433152eab674f49319e2c4c0240927b
Anthony Sciamanna <asciamanna@gmail.com>
2014-11-01 18:02:54 -0400

    removed unused variables from LibraryAlbumRepository

--- LastfmClient/Repositories/LibraryAlbumRepository.cs
+++ LastfmClient/Repositories/LibraryAlbumRepository.cs
@@ -3,15 +3,9 @@ using LastfmClient.Responses;
 
 namespace LastfmClient.Repositories {
   public class LibraryAlbumRepository : LibraryRepository {
-    private readonly IRestClient restClient;
-    private readonly ILibraryResponseParser parser;
-    
     public LibraryAlbumRepository(string apiKey) : this(apiKey, new RestClient(), new LibraryAlbumsResponseParser()) { }
 
-    public LibraryAlbumRepository(string apiKey, IRestClient restClient, ILibraryResponseParser parser) : base(apiKey, restClient, parser) {
-      this.restClient = restClient;
-      this.parser = parser;
-    }
+    public LibraryAlbumRepository(string apiKey, IRestClient restClient, ILibraryResponseParser parser) : base(apiKey, restClient, parser) { }
     
     protected override string BaseUri {
       get { return LastfmUri.LibraryAlbums; }
