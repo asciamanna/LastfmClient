422ca2581d3fdeb1426772af628dd46fb56437e7
Anthony Sciamanna <asciamanna@gmail.com>
2015-04-09 06:27:24 -0400

    unit test refactoring

--- LastfmClientTests/LastfmPageScraperTest.cs
+++ LastfmClientTests/LastfmPageScraperTest.cs
@@ -23,8 +23,8 @@ namespace LastfmClientTests {
     [Test]
     public void GetLastfmMusicSourceFromFile_While_User_Is_Not_Playing_Returns_Empty_Result() {
       var listeningFrom = scraper.GetLastfmMusicSourceFromFile(testFilePath + "lastfmUserPageNotCurrentlyPlaying.html");
-      Assert.That(String.IsNullOrWhiteSpace(listeningFrom.MusicServiceName), Is.True);
-      Assert.That(String.IsNullOrWhiteSpace(listeningFrom.MusicServiceUrl), Is.True);
+      Assert.That(listeningFrom.MusicServiceName, Is.Null.Or.Empty);
+      Assert.That(listeningFrom.MusicServiceUrl, Is.Null.Or.Empty);
     }
 
     [Test]
@@ -43,8 +43,8 @@ namespace LastfmClientTests {
     public void GetLastfmMusicSource_User_Page_Does_Not_Exist_Returns_Empty_Result() {
       var badPageUrl = @"http://www.last.fm/user/MadeUpNameDoesNotExistArgh";
       var result = scraper.GetLastfmMusicSource(badPageUrl);
-      Assert.That(String.IsNullOrWhiteSpace(result.MusicServiceName), Is.True);
-      Assert.That(String.IsNullOrWhiteSpace(result.MusicServiceUrl), Is.True);
+      Assert.That(result.MusicServiceName, Is.Null.Or.Empty);
+      Assert.That(result.MusicServiceUrl, Is.Null.Or.Empty);
     }
   }
 }
